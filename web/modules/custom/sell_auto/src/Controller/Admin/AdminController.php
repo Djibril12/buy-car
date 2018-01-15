a815c195f7840c632384ed123f3a93e6c01cf19d
bao <bao@bao.pc>
2018-01-15 23:32:06 +0100

    debut de la mise place du crud et du dashboard

---
+++ web/modules/custom/sell_auto/src/Controller/Admin/AdminController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\sell_auto\Controller\Admin;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class AdminController extends ControllerBase {
+
+
+  public function dashboard(){
+
+    return [
+      '#markup' => "Dashbord En construction"
+    ];
+  }
+
+  public function manageVehicules(){
+
+    
+
+
+    return [
+      '#markup' => "CRUD En construction"
+    ];
+  }
+
+}
