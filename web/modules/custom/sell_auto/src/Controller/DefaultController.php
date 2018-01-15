41aa20388ad1b02d50f3811b398c6a10673a29a6
bao <bao@bao.pc>
2018-01-15 22:57:32 +0100

    creation des tables vehicule et image

---
+++ web/modules/custom/sell_auto/src/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\sell_auto\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class DefaultController extends ControllerBase {
+
+
+  public function index(){
+
+    return [
+      '#markup' => "En construction",
+    ];
+  }
+
+}
